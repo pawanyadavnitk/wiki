8aeccaad158620910d19abc86b1e00982c1a33b8
Pawan Yadav <pawanyadav8053@gmail.com>
2021-06-07 17:50:58 +0530

    Add links in index page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia" # for adding namespace
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:name>", views.display_entry_page, name="display_entry_page")
