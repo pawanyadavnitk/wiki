b71952b539cb5bc5e16b97f98a53f7855b6f550b
Pawan Yadav <pawanyadav8053@gmail.com>
2021-06-07 17:22:22 +0530

    Add entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,6 @@
+import re
+import markdown
+
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +11,28 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def display_entry_page(request, name):
+    title, content = get_page_content(name)
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": content
+    })
+
+def get_page_content(name):
+    entries = util.list_entries()
+    is_page_present = False
+    for entry in entries:
+        # can use name.lowe() == entry.lower() as well
+        if re.fullmatch(entry, name, re.IGNORECASE):
+            name = entry
+            is_page_present = True
+            break
+
+    if is_page_present:
+        title = name
+        content = util.get_entry(name)
+        content = markdown.markdown(content)
+    else:
+        title = "Not Found"
+        content = "<h1>Page not found</h1>"
+    return title, content
